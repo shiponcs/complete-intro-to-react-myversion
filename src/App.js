7904ac9f48ed646bb22686354af3421038f2b797
mateen <admatinm@gmail.com>
2020-04-23 10:58:11 +0600

    Code splitting, a short note has been added as comment in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./searchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const themeHook = useState("peru");
@@ -16,10 +17,12 @@ const App = () => {
               <h1>Adopt Me</h1>
             </Link>
           </header>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense fallback={<h1>Loading! How are you btw?</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
@@ -27,3 +30,24 @@ const App = () => {
 };
 
 render(<App />, document.getElementById("root"));
+
+/* Here we used lazy, Suspense for code spliting.
+many time, we don't need to load all the source files at the beginning of the first loading the site,
+that time we use codes splitting, 
+in this file Details component is loading when we go to /details.
+by using lazy, and Suspense, we make it to load only when it's loaded.
+Test it out on network section of dev tools.
+you can also split the SearchParams
+
+*You shouldn't apply code splitting here unless the file is more than 30kb, in developement environment*
+we could make our files heavy by importing third party large packages.
+
+Again, you have to apply code splitting carefully, if you don't find heavy point
+to split your program, you are not supposed to apply code splitting, 
+because that makes your user wait on that particular portion unnecessarily
+that you could have loaded at beginning as that's lower in size.
+Keep in mind that, you can split javascript file, not it's necessary for that
+to be a Route, but again only when you see it's large. 
+Like the Context provider, you can hava Suspense in multiple places 
+or have one at top level and use it in the all components inside it 
+*/
